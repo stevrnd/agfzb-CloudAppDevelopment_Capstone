0390b051170a974843bc2828369afd3cc2ba1f16
stevrnd <steven_diep@hotmail.co.uk>
2024-01-08 12:16:20 -0500

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,8 +11,12 @@ urlpatterns = [
 
     # path for about view
 
+    path(route='about/', view=views.about, name='about'),
+
     # path for contact us view
 
+    path(route='contact/', view=views.contact, name='contact'),
+    
     # path for registration
 
     # path for login
